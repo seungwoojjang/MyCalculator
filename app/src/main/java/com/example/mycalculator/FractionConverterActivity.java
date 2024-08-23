f1c53eeff0235b1de9db9b0cb5d6c9b68d4d8bbf
flyw00 <xmfhfhfl@gmail.com>
2024-08-23 19:30:21 +0900

    2024 8 23

--- app/src/main/java/com/example/mycalculator/FractionConverterActivity.java
+++ app/src/main/java/com/example/mycalculator/FractionConverterActivity.java
@@ -1,6 +1,7 @@
 package com.example.mycalculator;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import java.lang.Integer;
 
 public class FractionConverterActivity extends AppCompatActivity {
     EditText numerator1;
@@ -39,5 +41,40 @@ public class FractionConverterActivity extends AppCompatActivity {
 
         btnConvert1 = findViewById(R.id.btnConvert1);
         btnConvert2 = findViewById(R.id.btnConvert2);
+
+        btnConvert1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (numerator1.getText().toString().compareTo("") != 0 && denominator1.getText().toString() != " ") {
+                    int gcd = GCD(Integer.parseInt(numerator1.getText().toString()), Integer.parseInt(denominator1.getText().toString()));
+                    numerator3.setText("" + (Integer.parseInt(numerator1.getText().toString()) / gcd));
+                    denominator3.setText("" + (Integer.parseInt(denominator1.getText().toString()) / gcd));
+                }
+                if (numerator2.getText().toString().compareTo("") != 0 && denominator2.getText().toString() != " ") {
+                    int gcd = GCD(Integer.parseInt(numerator1.getText().toString()), Integer.parseInt(denominator2.getText().toString()));
+                    numerator4.setText("" + (Integer.parseInt(numerator2.getText().toString()) / gcd));
+                    denominator4.setText("" + (Integer.parseInt(denominator2.getText().toString()) / gcd));
+                }
+            }
+        });
+
+    }
+
+    int GCD(int a,int b){
+        int min = 0;
+        if(a > b)
+            min = b;
+        else
+            min = a;
+        for(int i = min;i >=1; i --) {
+            if (a % i == 0 && b % i == 0) {
+                return i;
+            }
+        }
+        return 1;
+    }
+    int LCM(int a,int b){
+        int gcd = GCD(a,b);
+        return a*b / gcd;
     }
 }
