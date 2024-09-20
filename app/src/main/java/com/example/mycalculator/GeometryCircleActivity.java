2c5df3b562eba9daf1e5d05c2f2838183644ab7c
flyw00 <xmfhfhfl@gmail.com>
2024-09-20 19:30:44 +0900

    2024 9 20

--- app/src/main/java/com/example/mycalculator/GeometryCircleActivity.java
+++ app/src/main/java/com/example/mycalculator/GeometryCircleActivity.java
@@ -1,6 +1,10 @@
 package com.example.mycalculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +13,23 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class GeometryCircleActivity extends AppCompatActivity {
-
+    Button btnCalculate;
+    EditText editRadius;
+    TextView TextResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geometry_circle);
+        btnCalculate = findViewById(R.id.btnCircleResult);
+        editRadius = findViewById(R.id.editCircleR);
+        TextResult = findViewById(R.id.textCircleResult);
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double R = Integer.parseInt(editRadius.getText().toString());
+                double result = R*R*3.141592;
+                TextResult.setText(""+result);
+            }
+        });
     }
 }
